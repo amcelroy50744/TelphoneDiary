9c296c4ee80ff0d8145663f9ad1580b55c91c696
amcelroy50744 <85271297+amcelroy50744@users.noreply.github.com>
2021-09-23 10:33:12 -0400

    Add files via upload

---
+++ TelephoneDiary/dbo.Table.data.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO [dbo].[Table] ([First], [Last], [Mobile], [Email], [Catagory]) VALUES (N' Andre', N' McElroy ', N' 256-337-0091 ', N' amcelroy50744@gmail.com ', N' Home ')
+INSERT INTO [dbo].[Table] ([First], [Last], [Mobile], [Email], [Catagory]) VALUES (N' Alona', N' Goram ', N' 256-358-5929 ', N' alona@gmail.com ', N'  ')
+INSERT INTO [dbo].[Table] ([First], [Last], [Mobile], [Email], [Catagory]) VALUES (N' Reggie', N' Neal ', N' 423-637-1745 ', N' reggie@gmail.com ', N' Home ')
+INSERT INTO [dbo].[Table] ([First], [Last], [Mobile], [Email], [Catagory]) VALUES (N' Jeremy ', N' Huddleston ', N' 555-5555-555 ', N' jhud@trueCoder.com ', N' Home ')
